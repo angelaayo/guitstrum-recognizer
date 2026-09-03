3fcf517be27e4b578629e3f17d00fd13f2e0ee6c
Angela <angelaayodeji@gmail.com>
2026-09-02 21:58:20 -0500

    added webscoket to prediction connection for live audio capture

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, WebSocket, WebSocketDisconnect
 from pydub import AudioSegment
 from fastapi.middleware.cors import CORSMiddleware
 import librosa
 import numpy as np
 import joblib
 import io
+
+TARGET_SR = 22050
+BROWSER_SR = 44100
+BUFFER_SECONDS = 5
 
 app = FastAPI()
 
@@ -17,6 +21,17 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+def getChord(y, sr):
+    S = librosa.feature.melspectogram(y=y, sr=sr, n_mels=128)
+    S_db_mel = librosa.amplitude_to_db(S, ref=np.max)
+    features = S_db_mel.reshape(1, -1)
+    prediction = model.predict(features)
+    chord = encoder.inverse_transform(prediction)[0]
+
+    return chord
+
+
 
 @app.post("/recognize")
 async def recognize(audio: UploadFile = File(...)):
@@ -43,12 +58,46 @@ async def recognize(audio: UploadFile = File(...)):
     else:
         padding = target_samples - len(y_trimmed)
         y_fixed = np.pad(y_trimmed, (0, padding), mode="constant")
-    import soundfile as sf
-    sf.write("debug_final.wav", y_trimmed, sr)
-    # S = librosa.feature.melspectrogram(y=y_fixed, sr=sr, n_mels=128)
-    # S_db_mel = librosa.amplitude_to_db(S, ref=np.max)
-    # features = S_db_mel.reshape(1, -1)
-    # prediction = model.predict(features)
-    # chord = encoder.inverse_transform(prediction)[0]
+    # import soundfile as sf
+    # sf.write("debug_final.wav", y_trimmed, sr)
+    S = librosa.feature.melspectrogram(y=y_fixed, sr=sr, n_mels=128)
+    S_db_mel = librosa.amplitude_to_db(S, ref=np.max)
+    features = S_db_mel.reshape(1, -1)
+    prediction = model.predict(features)
+    chord = encoder.inverse_transform(prediction)[0]
 
-    # return {"detectedChord": chord}
+    return {"detectedChord": chord}
+
+@app.websocket("/ws/recognize")
+async def ws_recognize(websocket: WebSocket):
+    await websocket.accept()
+    print("Client connected")
+
+    audio_buffer = np.array([], dtype=np.float32)
+
+    try:
+        while True:
+            data = await websocket.receive_bytes()
+            chunk = np.frombuffer(data, dtype=np.float32)
+            audio_buffer - np.concatenate([audio_buffer, chunk])
+
+            #trying to aquire out 5 second window
+
+            if len(audio_buffer) >= BROWSER_SR * BUFFER_SECONDS:
+                window = audio_buffer[: BROWSER_SR * BUFFER_SECONDS]
+                audio_buffer = audio_buffer[BROWSER_SR * BUFFER_SECONDS:]
+                try:
+                    y_resampled = librosa.resample(window, orig_sr=BROWSER_SR, target_sr=TARGET_SR)
+                    target_samples = BUFFER_SECONDS * TARGET_SR
+                    if len(y_resampled) > target_samples:
+                        y_fixed = y_resampled[:target_samples]
+                    else:
+                        padding = max(0, target_samples - len(y_resampled))
+                        y_fixed = np.pad(y_resampled, (0, padding), mode="constant")
+                    chord  = getChord(y_fixed, TARGET_SR)
+                    print(f"Predicted: {chord}")
+                    await websocket.send_json({"detectedChord": chord})
+                except Exception as e:
+                    print(f"Prediction error: {e}")
+    except WebSocketDisconnect:
+        print("Client disconnected")
