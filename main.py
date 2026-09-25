cc343fe46d9694edc0fcb0e2c4fd73d91cf0d61e
Angela <angelaayodeji@gmail.com>
2026-09-24 21:39:18 -0500

    added healh status to main.py

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ CONFIDENCE_FLOOR = 0.45
 
 app = FastAPI()
 
+@app.get("/health")
+def health():
+    return{"status": "ok"}
+
 model = joblib.load("chord_model.pkl")
 encoder = joblib.load("chord_encoder.pkl")
 app.add_middleware(
@@ -38,36 +42,38 @@ def getChord(y, sr):
 
 
 
-@app.post("/recognize")
-async def recognize(audio: UploadFile = File(...)):
-    #ensuring our audio is processed in the right format
-    raw_bytes = await audio.read()
-    audio_segment = AudioSegment.from_file(io.BytesIO(raw_bytes))
-    wav_buffer = io.BytesIO()
-    audio_segment.export(wav_buffer, format="wav")
-    wav_buffer.seek(0)
+# @app.post("/recognize")
+# async def recognize(audio: UploadFile = File(...)):
+#     #ensuring our audio is processed in the right format
+#     raw_bytes = await audio.read()
+#     audio_segment = AudioSegment.from_file(io.BytesIO(raw_bytes))
+#     wav_buffer = io.BytesIO()
+#     audio_segment.export(wav_buffer, format="wav")
+#     wav_buffer.seek(0)
 
-    #based on our training on mel spectograms all our audio files need to be
-    #the same length of 5 seconds
+#     #based on our training on mel spectograms all our audio files need to be
+#     #the same length of 5 seconds
 
-    target_duaration = 5
-    y,sr = librosa.load(wav_buffer)
-    #trim off silence in audio
-    print(f"Raw audio: {len(y)/sr:.2f}s, sample rate: {sr}")
-    y_trimmed, _ = librosa.effects.trim(y, top_db=34)
-    print(f"After trim: {len(y_trimmed)/sr:.2f}s")
+#     target_duaration = 5
+#     y,sr = librosa.load(wav_buffer)
+#     #trim off silence in audio
+#     print(f"Raw audio: {len(y)/sr:.2f}s, sample rate: {sr}")
+#     y_trimmed, _ = librosa.effects.trim(y, top_db=34)
+#     print(f"After trim: {len(y_trimmed)/sr:.2f}s")
 
-    target_samples = target_duaration * sr
-    if len(y_trimmed) > target_samples:
-        y_fixed = y_trimmed[:target_samples]
-    else:
-        padding = target_samples - len(y_trimmed)
-        y_fixed = np.pad(y_trimmed, (0, padding), mode="constant")
-    # import soundfile as sf
-    # sf.write("debug_final.wav", y_trimmed, sr)
-    chord, confidence = getChord(y_fixed, sr)
+#     target_samples = target_duaration * sr
+#     if len(y_trimmed) > target_samples:
+#         y_fixed = y_trimmed[:target_samples]
+#     else:
+#         padding = target_samples - len(y_trimmed)
+#         y_fixed = np.pad(y_trimmed, (0, padding), mode="constant")
+#     # import soundfile as sf
+#     # sf.write("debug_final.wav", y_trimmed, sr)
+#     chord, confidence = getChord(y_fixed, sr)
 
-    return {"detectedChord": chord, "confidence": confidence}
+#     return {"detectedChord": chord, "confidence": confidence}
+
+
 
 
 #overall process that got things working:
